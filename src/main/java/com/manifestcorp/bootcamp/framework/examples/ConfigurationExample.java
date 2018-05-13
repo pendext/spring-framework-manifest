4c49a7e9827cc5260926ac05939ed5553dcdcf10
john p <john.pendexter@gmail.com>
2018-05-13 14:19:48 -0400

    Initial configuration test

---
+++ src/main/java/com/manifestcorp/bootcamp/framework/examples/ConfigurationExample.java
@@ -0,0 +1,12 @@
+package com.manifestcorp.bootcamp.framework.examples;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConfigurationExample {
+
+    public String getPropertyFromEnvironmentVariable(String key) {
+        return null;
+    }
+
+}
---
+++ src/test/java/com/manifestcorp/bootcamp/framework/examples/ConfigurationExampleTest.java
@@ -0,0 +1,25 @@
+package com.manifestcorp.bootcamp.framework.examples;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ConfigurationExampleTest {
+
+    @Autowired
+    ConfigurationExample configurationExample;
+
+    @Test
+    public void environmentVariableExample() {
+        String expectedValue = "expectedValue";
+        String expectedKey = "expectedKey";
+
+        assertThat(expectedValue).isEqualTo(configurationExample.getPropertyFromEnvironmentVariable(expectedKey));
+    }
+}
