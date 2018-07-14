6ee3b92337167c0ae66cea44acb386ea85f2c3ff
john p <john.pendexter@gmail.com>
2018-07-14 15:44:03 -0400

    Removing whitespace that was irritating me

--- src/main/java/com/manifestcorp/bootcamp/framework/examples/ConfigurationExample.java
+++ src/main/java/com/manifestcorp/bootcamp/framework/examples/ConfigurationExample.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class ConfigurationExample {
 
-
     String property;
 
     public String getPropertyFromEnvironmentVariable(String key) {
